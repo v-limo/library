1ed82aa2cc4f0b3b59a52b5c9213db7523b28f61
Vincent Limo <limovincenti@gmail.com>
2023-03-05 12:38:29 +0200

    ft: add ApplicationDbContext

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using library.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>().ToTable("Authorss");
+        modelBuilder.Entity<Book>().ToTable("Books");
+        modelBuilder.Entity<User>().ToTable("Users");
+    }
+}
