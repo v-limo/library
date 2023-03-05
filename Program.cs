34a5666b99c399dce05f118efde9b5a16683978f
Vincent Limo <limovincenti@gmail.com>
2023-03-05 15:54:28 +0200

    ft: add dependency injection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using library.Repositories.Implementations;
+using library.Repositories.Interfaces;
+using library.Service.Implementations;
+using library.Service.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<
+ApplicationDbContext
+>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBookService, BookService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
 
 var app = builder.Build();
 
